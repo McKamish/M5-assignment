19967c16ea4554329908d2626cedad5b655a0aa6
Cory Jameson <corymjameson@gmail.com>
2026-07-07 12:45:44 -0700

    progressing through the form.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// GLOBAL VARIABLES
+let identText
+let name
+let extension
+let email
+let department
+let addEmployeeButton
+
+
+// GET DOM ELEMENTS
+identText = document.getElementById("ident")
+addEmplyeeButton = document.getElementById("submit")
+
+
+// do we just print stuff here? with console.log?
+
+
+
+// HANDLE CLICK OF ADD EMPLOYEE BUTTON
+addEmployeeButton.addEventListener('click', () => {
+    //do the calculations here
+})
+
+
+
+
+//let btn1 = document.getElementById("btn1")
+//btn1.onclick = () => alert("Hello World")
+//3 ways to do the same thing:
+//1. btn1.addEventListener('click', displayMessage)
+//function displayMessage() {
+//    alert("Hello World")
+//}
+
+//2. btn1.addEventListener('click', () => {
+//    alert("Hello Wortld")
+//})
+
+//3. btn1.addEventListener('mouseover', () => alert('Hello world'))
